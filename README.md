666f7cefbc3c1c867e28f4f2502d7dc42a4fda3e
mtjuney <mtjuney@gmail.com>
2016-06-20 08:32:00 +0900

    wikidataのいらない箇所をエスケープ

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NEologdのメタデータ収集
+
+1. neologdの辞書データを解凍し，`neologd_data/`に置く．
+1. wikipediaのダンプデータ(`jawiki-latest-pages-article.xml.bz2`もしくはその軽量版)を`wiki_data`に置く．
+1. `python make_neologd_vocab.py`を実行する．第一引数には入力データ(neologdの辞書データ.csv)，第二引数には`middle_data/neologd_vocab.csv`を指定する．
+1. `middle_data/`の中に`wiki_data_text/`を作成．
+1. `wp2txt -i jawiki-latest-pages-articles.xml.bz2 -o middle_data/wiki_data_text/ --no-list --no-heading --no-title --no-marker`を実行．`-i`オプションはwikipediaのダンプデータを指定すること．
+1. `cat middle_data/wiki_data_text/* > middle_data/wiki_data_text.txt`を実行する．
+1. `python make_wiki_wakachi.py`を実行する．第一引数には`middle_data/wiki_data_text.txt`，第二引数には`middle_data/wiki_wakachi.txt`を指定する．
+1. `python make_synonym_data.py -i middle_data/wiki_wakachi.txt -o data/synonym_data.
+tsv -v middle_data/neologd_vocab.csv`を実行する．`-s, --size`オプションで単語ベクトルの出力次元を，`-m, --min_count`で単語を語彙に加える最低限の出現回数を，`-w, --window`で周辺分布を見る窓の大きさを指定できる．デフォルトは`size=100, min_count=5, window=5`．
--- make_wiki_wakachi.py
+++ make_wiki_wakachi.py
@@ -1,16 +1,25 @@
 from natto import MeCab
 import sys
 import re
+import argparse
 
-line_num_total = sum(1 for line in open(sys.argv[1], 'r'))
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--dicdir', default='/usr/lib/mecab/dic/mecab-ipadic-neologd')
+parser.add_argument('-i', '--input', default='middle_data/wiki_data_text.txt')
+parser.add_argument('-o', '--output', default='middle_data/wiki_wakachi.txt')
+args = parser.parse_args()
+
+line_num_total = sum(1 for line in open(args.input, 'r'))
 disp_num = line_num_total // 100
 print('line_num_total', line_num_total)
 
 # 入力ファイル，出力ファイルを開き，MeCabを呼び出す
 # dicdirは処理するサーバ用
-ops={'dicdir': '/usr/lib/mecab/dic/mecab-ipadic-neologd', 'node_format': '%f[6]'}
-with open(sys.argv[1], 'r') as f_in, open(sys.argv[2], 'w') as f_out, MeCab(options=ops) as nm:
+ops={'dicdir': args.dicdir, 'node_format': '%f[6]'}
+with open(args.input, 'r') as f_in, open(args.output, 'w') as f_out, MeCab(options=ops) as nm:
     line_num = 0
+    escape_flag = False
+    escape_count = 0
     for line in f_in:
         line_num += 1
         if line_num % disp_num == 0:
@@ -18,6 +27,18 @@ with open(sys.argv[1], 'r') as f_in, open(sys.argv[2], 'w') as f_out, MeCab(opti
 
         if line == '\n':
             continue
+        elif re.match(r'^\{\{', string=line):
+            escape_flag = True
+            escape_count += 1
+            continue
+        elif escape_flag:
+            if re.search(r'\}\}\n', string=line) or escape_count > 100:
+                escape_flag = False
+                escape_count = 0
+
+            escape_count += 1
+            continue
+
         line_text = re.sub(r'[\s　]+', '', line.strip())
         nodes = nm.parse(line_text, as_nodes=True)
         mors = []
