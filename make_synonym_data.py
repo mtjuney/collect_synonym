ab91a8022a813cfe468983e39757676c2a68a0d8
mtjuney <mtjuney@gmail.com>
2016-06-19 21:38:40 +0900

    add synonym data

--- make_synonym_data.py
+++ make_synonym_data.py
@@ -7,11 +7,15 @@ model = word2vec.Word2Vec.load(sys.argv[1])
 with open(sys.argv[2], 'r', newline='') as f:
     reader = csv.reader(f)
     for row in reader:
-        neologd_vocab = row
+        neologd_vocab = frozenset(row)
         break
 
+neologd_vocab_num = len(neologd_vocab)
+
 with open(sys.argv[3], 'w') as f:
-    count = 0
+    hit_count = 0
+    neologd_vocab_count = 0
+    disp_num = neologd_vocab_num // 100
     for word in neologd_vocab:
         try:
             synonyms = model.most_similar(positive=[word], negative=[], topn=5)
@@ -21,7 +25,12 @@ with open(sys.argv[3], 'w') as f:
             output_line = word + '\t'
             for synonym in synonyms:
                 output_line += synonym[0] + '\t' + str(synonym[1]) + '\t'
-
             output_line = output_line[:-1] + '\n'
-            if count > 10:
-                break
+            f.write(output_line)
+            hit_count += 1
+        finally:
+            neologd_vocab_count += 1
+            if neologd_vocab_count % disp_num == 0:
+                print('{}\t{} / {}\t{}'.format(hit_count, neologd_vocab_count, neologd_vocab_num, neologd_vocab_count / neologd_vocab_num))
+            
+            
